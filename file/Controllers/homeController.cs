1cb70dac7627db4948a913dcba4b827cda33e56c
aqw42 <garsvog1@gmail.com>
2020-01-29 12:21:54 +0100

    done

---
+++ file/Controllers/homeController.cs
@@ -0,0 +1,30 @@
+﻿using file.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace file.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCEntities obj = new MVCEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+
+            string fileName = Path.GetFileName(file.FileName);
+            string path = Path.Combine(Server.MapPath("~/Img"), fileName);
+
+            file.SaveAs(path);
+            ViewBag.path = path;
+            return View();
+        }
+    }
+}
